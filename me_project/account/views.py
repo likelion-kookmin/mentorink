56834e348e1ce61200f471b9041360f37f4f083a
cychann <skcbdlhc129@naver.com>
2021-05-22 21:31:08 +0900

    로그인, 로그아웃 기능 구현

---
+++ me_project/account/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(
+                request=request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+
+            return redirect("main")
+
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("main")
--- me_project/me_project/settings.py
+++ me_project/me_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'me_app',
+    'account',
 ]
 
 MIDDLEWARE = [
