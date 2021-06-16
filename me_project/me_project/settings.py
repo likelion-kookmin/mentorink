1a1f8cb9f7de8f296386a1b85c7d20426d19c452
JunYoung0131 <wnsdud5003@gmail.com>
2021-06-16 12:54:59 +0900

    add bootstrap

--- me_project/me_project/settings.py
+++ me_project/me_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'me_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["me_project/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
