94886461a2caabb9e10c59263e5775e62c34bd3b
신현지 <ab9943@naver.com>
2021-06-23 20:44:44 +0900

    update

--- me_project/me_app/views.py
+++ me_project/me_app/views.py
@@ -31,12 +31,13 @@ def new(request):
 
 
 def create(request):
-    new_idea = Idea()
-    new_idea.title = request.POST['title']
-    new_idea.writer = request.POST['writer']
-    new_idea.body = request.POST['body']
-    new_idea.pud_date = timezone.now()
-    new_idea.save()
+    if(request.method == 'POST'):
+        new_idea = Idea()
+        new_idea.title = request.POST['title']
+        new_idea.writer = request.POST['writer']
+        new_idea.body = request.POST['body']
+        new_idea.pud_date = timezone.now()
+        new_idea.save()
     return redirect('detail', new_idea.id)
 
 
