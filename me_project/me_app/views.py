0b2aa04351e2e001b4a22f98d97d2f1948f33514
JunYoung0131 <wnsdud5003@gmail.com>
2021-06-30 11:27:02 +0900

    페이지네이션 추가

--- me_project/me_app/views.py
+++ me_project/me_app/views.py
@@ -1,3 +1,4 @@
+from django_filters import filters
 from .models import Comment, Idea
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
@@ -7,6 +8,8 @@ from account.models import UserModel
 from account.forms import RegisterForm
 from django.db import models
 from django.conf import settings
+from django.core.paginator import Paginator
+
 
 def main(request):
     return render(request, 'main.html')
@@ -23,24 +26,43 @@ def join(request):
 def mypage(request):
     return render(request, 'mypage.html')
 
+
 def myinfo(request):
-    return render(request,'myinfo.html')
+    return render(request, 'myinfo.html')
+
 
 def myq(request):
     myquest = request.POST.get('myquest')
     ideas = Idea.objects.filter(writer=request.user.username)
-    return render(request,'myq.html',{'ideas': ideas})
+    return render(request, 'myq.html', {'ideas': ideas})
+
 
 def myc(request):
     mycomment = request.POST.get('mycomment')
-    comments=Comment.objects.filter(author=request.user)
-    return render(request,'myc.html',{'comments': comments})
+    comments = Comment.objects.filter(author=request.user)
+    return render(request, 'myc.html', {'comments': comments})
+
 
 def list(request):
-    search = request.POST.get('search')
+    result = request.GET.get('search')
+    if(result.find('?') == -1):
+        search = request.GET.get('search')
+        page = request.GET.get('page', '1')  # 페이지
+    else:
+        sp = result.split('?')
+        search = sp[0]
+        page = sp[1].split('=')[1]
+
     ideas = Idea.objects.filter(title__contains=search)
-    count=ideas.count()
-    return render(request, 'list.html', {'ideas': ideas, 'search':search, 'count':count})
+    count = ideas.count()
+
+    paginator = Paginator(ideas, 10)  # 페이지당 10개씩 보여주기
+
+    page_obj = paginator.get_page(page)
+
+    return render(request, 'list.html', {'ideas': page_obj, 'search': search, 'count': count})
+
+# return render(request, 'list.html', {'ideas': ideas, 'search': search, 'count': count, 'context': context})
 
 
 def new(request):
@@ -49,17 +71,17 @@ def new(request):
 
 def create(request):
     if(request.method == 'POST'):
-         new_idea = Idea()
-         new_idea.title = request.POST['title']
-         new_idea.writer = request.POST['writer']
-         new_idea.body = request.POST['body']
-         new_idea.pud_date = timezone.now()
-         
-         if not request.FILES:
+        new_idea = Idea()
+        new_idea.title = request.POST['title']
+        new_idea.writer = request.POST['writer']
+        new_idea.body = request.POST['body']
+        new_idea.pud_date = timezone.now()
+
+        if not request.FILES:
             new_idea.image = ""
             new_idea.save()
-            
-         else:
+
+        else:
             new_idea.image = request.FILES['image']
             new_idea.save()
 
@@ -77,19 +99,23 @@ def update(request, idea_id):
         edit_idea.save()
     return redirect('detail', edit_idea.id)
 
+
 def delete(request, idea_id):
-    delete_idea= get_object_or_404(Idea, pk=idea_id)
+    delete_idea = get_object_or_404(Idea, pk=idea_id)
     delete_idea.delete()
     return redirect('main')
 
+
 def detail(request, idea_id):
     idea = get_object_or_404(Idea, pk=idea_id)
-    comments = Comment.objects.filter(idea = idea)
-    return render(request, 'detail.html', {'idea': idea, 'comments' : comments})
+    comments = Comment.objects.filter(idea=idea)
+    return render(request, 'detail.html', {'idea': idea, 'comments': comments})
+
 
 def edit(request, idea_id):
     idea = get_object_or_404(Idea, pk=idea_id)
-    return render(request, 'edit.html', {'idea':idea})
+    return render(request, 'edit.html', {'idea': idea})
+
 
 def add_comment_to_idea(request, idea_id):
     idea = get_object_or_404(Idea, pk=idea_id)
